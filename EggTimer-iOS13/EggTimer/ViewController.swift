09930707cbcffdcd7f656f741047ff2adfa09a38
hasitheyepax <hasith.m@eyepax.com>
2022-07-27 23:46:27 +0530

    Using a timer in swift with optionals

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,21 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var eggTimes = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard": 12
+    ]
+    
+    var remainingSeconds: Int = 0
+    
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
         let hardness = sender.currentTitle
         
-        if (hardness == "Soft") {
-            print(softTime)
-        } else if (hardness == "Medium") {
-            print(mediumTime)
-        } else {
-            print(hardTime)
-        }
+//        if (hardness == "Soft") {
+//            print(softTime)
+//        } else if (hardness == "Medium") {
+//            print(mediumTime)
+//        } else {
+//            print(hardTime)
+//        }
+        
+        remainingSeconds = eggTimes[hardness!]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countdownTimer), userInfo: nil, repeats: true)
+        
     }
     
+    @objc func countdownTimer() {
+        if(remainingSeconds > 0) {
+            print("\(remainingSeconds) seconds.")
+            remainingSeconds -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        }
+    }
 
 }
